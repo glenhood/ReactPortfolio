34e16496fac2062f7c47ecbd25cc26253e8d472d
Glen Hood <glenhoodatx@gmail.com>
2021-08-26 16:30:18 -0500

    added portfolio page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,48 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Project1 from '../styles/images/2.png';
-import Project2 from '../styles/images/me.jpeg';
-import Project3 from '../styles/images/me.jpeg';
-import Project4 from '../styles/images/me.jpeg';
-import Project5 from '../styles/images/me.jpeg';
-import { Card } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import Project2 from '../styles/images/1.png';
+import Project3 from '../styles/images/3.png';
+import { Card, CardGroup, Row, Button } from 'react-bootstrap';
 
 export default function Projects() {
   return (
-<Card style={{ width: '500px' }}>
-  <Card.Img variant="top" src= { Project1 } />
-  <Card.Body>
-    <Card.Title>Meet Up!</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Link to site!</Button>
-  </Card.Body>
-</Card>
-  );
-}{
-(
-<Card style={{ width: '500px' }}>
-  <Card.Img variant="top" src= { Project1 } />
-  <Card.Body>
-    <Card.Title>Meet Up!</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Link to site!</Button>
-  </Card.Body>
-</Card>
-  );
-}
+<Row lgxx={3} className="g-3">
+<CardGroup>
+  <Card>
+    <Card.Img className="one" variant="top" src={ Project1 } />
+    <Card.Body>
+      <Card.Title>Meet Up!</Card.Title>
+      <Card.Text>
+        An app designed for a user to connect with other users about personally orginized events.
+      </Card.Text>
+      <Button href="https://uta-meet-up.herokuapp.com/" variant="primary">Link to site!</Button>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Sequelize/Handlebars</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={ Project2 } />
+    <Card.Body>
+      <Card.Title>CryptoCorner</Card.Title>
+      <Card.Text>
+        An educational site for the beginner Cryto trader. Uses 3rd Party API's for realtime crypto information
+      </Card.Text>
+      <Button href="https://glenhood.github.io/Project-1/" variant="primary">Link to site!</Button>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">JavaScript/API</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={ Project3 } />
+    <Card.Body>
+      <Card.Title>Band Mates (coming soon!)</Card.Title>
+      <Card.Text>
+        A social media platform that connects musicians with other musicians, looking to either start a band or perform at a gig.
+      </Card.Text>
+      <Button href="" variant="primary">Link to site!</Button>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">React</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+</Row>
 
+  )}
